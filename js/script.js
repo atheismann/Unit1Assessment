75149b3ecaaf4ac83d79d6b076e5a56c86e1f50b
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-09 10:38:25 -0500

    update all files

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let msgEl = document.getElementById('msg');
+let incBtn = document.getElementById('increase');
+let decBtn = document.getElementById('decrease');
+let num = document.getElementById('number');
+let sum = 0;
+
+incBtn.addEventListener('click', handleAdd);
+decBtn.addEventListener('click', handleSub);
+
+function handleAdd(){
+    sum += parseInt(num.value);
+    render();
+};
+
+function handleSub(){
+    sum -= parseInt(num.value);
+    render();
+};
+
+function render(){
+  msgEl.textContent = sum;
+};
+
+render();
